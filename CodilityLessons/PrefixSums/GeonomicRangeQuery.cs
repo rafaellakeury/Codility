e7391cd38d051626570740577956c48363b57726
Rafa <rafaella_souza@gap.com>
2019-09-07 21:33:40 -0300

    Fix GeonomicRangeQuery logic

--- CodilityLessons/PrefixSums/GeonomicRangeQuery.cs
+++ CodilityLessons/PrefixSums/GeonomicRangeQuery.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace CodilityLessons.PrefixSums
 {
@@ -8,7 +7,7 @@ namespace CodilityLessons.PrefixSums
 	{
 		public int[] Solution(String S, int[] P, int[] Q)
 		{
-			List<int> result = new List<int>();
+			//'GT', [0, 0, 1], [0, 1, 1]
 			var nucleotidesDictionary = new Dictionary<string, int>()
 			{
 				{"A", 1},
@@ -16,15 +15,71 @@ namespace CodilityLessons.PrefixSums
 				{"G", 3},
 				{"T", 4}
 			};
-			var dnaSequence = S.ToCharArray(); //CAGCCTA
+			List<int> result = new List<int>();
+			int[] aCounter = new int[S.Length];
+			int[] cCounter = new int[S.Length];
+			int[] gCounter = new int[S.Length];
+			int[] tCounter = new int[S.Length];
+
+
+			var dnaSequence = S.ToCharArray(); //GT
+
+			int aCount = 0; int cCount = 0; int gCount = 0; int tCount = 0;
+			for (int i = 0; i < dnaSequence.Length; i++)
+			{
+				if (dnaSequence[i].ToString().Equals("A"))
+				{
+					aCount++;
+				}
+				else if (dnaSequence[i].ToString().Equals("C"))
+				{
+					cCount++;
+				}
+				else if (dnaSequence[i].ToString().Equals("G"))
+				{
+					gCount++;
+				}
+				else
+				{
+					tCount++;
+				}
+				aCounter[i] = aCount;
+				cCounter[i] = cCount;
+				gCounter[i] = gCount;
+				tCounter[i] = tCount;
+			}
 
 			for (int i = 0; i < P.Length; i++)
 			{
-				var subArray = dnaSequence.ToList().GetRange(P[i], Q[i]);
-				var smaller = subArray.OrderBy(x => x).ToList().First();
-				result.Add(smaller);
+				var valToAdd = 0;
+				var from = P[i] > 0 ? P[i] - 1 : P[i];
+				var to = Q[i];
+				if (P[i] == Q[i])
+				{
+					var letra = dnaSequence[P[i]].ToString();
+					valToAdd = nucleotidesDictionary[letra];
+				}
+				else if (aCounter[to] - aCounter[from] > 0 || (P[i] == 0 && aCounter[to] > 0))
+				{
+					valToAdd = 1;
+				}
+				else if (cCounter[to] - cCounter[from] > 0 || (P[i] == 0 && cCounter[to] > 0))
+				{
+					valToAdd = 2;
+				}
+				else if (gCounter[to] - gCounter[from] > 0 || (P[i] == 0 && gCounter[to] > 0))
+				{
+					valToAdd = 3;
+				}
+				else
+				{
+					valToAdd = 4;
+				}
+
+				result.Add(valToAdd);
 			}
 			return result.ToArray();
 		}
+
 	}
 }
