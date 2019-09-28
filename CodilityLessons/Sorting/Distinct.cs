97f684758934c929bcc1633b29b0e03174c6d5d4
Rafa <rafaella_souza@gap.com>
2019-09-28 20:55:52 -0300

    Add Distinct solution

---
+++ CodilityLessons/Sorting/Distinct.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace CodilityLessons.Sorting
+{
+	public class Distinct
+	{
+		public int Solution(int[] A)
+		{
+			return A.Distinct().Count();
+		}
+	}
+}
---
+++ CodilityLessonsTests/Sorting/DistinctTests.cs
@@ -0,0 +1,24 @@
+﻿using CodilityLessons.Sorting;
+using NUnit.Framework;
+
+namespace CodilityLessonsTests.Sorting
+{
+	[TestFixture]
+	class DistinctTests
+	{
+		[Test]
+		public void TestBasicCase()
+		{
+			//Given
+			var baseCaseArray = new int[] { 2, 1, 1, 2, 3, 1 };
+			var distinct = new Distinct();
+			var expectedResult = 3;
+
+			//When
+			var actualResult = distinct.Solution(baseCaseArray);
+
+			//Then
+			Assert.AreEqual(expectedResult, actualResult);
+		}
+	}
+}
