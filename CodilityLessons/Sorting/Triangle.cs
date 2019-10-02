1a5ee96933dc3242c031ed85023dfa09c0feae6f
Rafa <rafaella_souza@gap.com>
2019-10-02 16:39:27 -0300

    Add Triangle exercise solution

---
+++ CodilityLessons/Sorting/Triangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace CodilityLessons.Sorting
+{
+	public class Triangle
+	{
+		public int Solution(int[] A)
+		{
+			if (A.Count() < 3 || A == null)
+				return 0;
+			Array.Sort(A);
+			for(int i=0; i<A.Length-2;i++)
+			{
+				if (A[i] >= 0 && A[i] > A[i + 2] - A[i + 1])
+					return 1;
+			}
+			return 0;
+		}
+	}
+}
---
+++ CodilityLessonsTests/Sorting/TriangleTests.cs
@@ -0,0 +1,40 @@
+﻿using CodilityLessons.Sorting;
+using NUnit.Framework;
+
+namespace CodilityLessonsTests.Sorting
+{
+	[TestFixture]
+
+	class TriangleTests
+	{
+		[Test]
+		public void TestBasicCaseReturns1()
+		{
+			//Given
+			var baseCaseArray = new int[] {10, 2, 5, 1, 8, 20};
+			var triangle = new Triangle();
+			var expectedResult = 1;
+
+			//When
+			var actualResult = triangle.Solution(baseCaseArray);
+
+			//Then
+			Assert.AreEqual(expectedResult, actualResult);
+		}
+
+		[Test]
+		public void TestBasicCaseReturns0()
+		{
+			//Given
+			var baseCaseArray = new int[] { 10, 50, 5, 1 };
+			var triangle = new Triangle();
+			var expectedResult = 0;
+
+			//When
+			var actualResult = triangle.Solution(baseCaseArray);
+
+			//Then
+			Assert.AreEqual(expectedResult, actualResult);
+		}
+	}
+}
